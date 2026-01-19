b3fe5ec82124e94680f0b9b09598a6d0c5ad956a
deeptaxfile-stack <deep.taxfile@gmail.com>
2026-01-19 17:29:11 +0530

    just try 1

---
+++ app.js
@@ -0,0 +1,61 @@
+let data = [];
+let editId = null;
+
+function readAll() {
+  let tableData = document.querySelector(".table_data");
+  var e = "";
+
+  data.map(
+    (obj) =>
+      (e += `<tr>
+            <td>${obj.name}</td>
+            <td>
+                <button onclick=edit(${obj.id})>Edit</button>
+                <button onclick=remove(${obj.id})>Delete</button>
+            </td>
+        </tr>`),
+  );
+  tableData.innerHTML = e;
+}
+
+function edit(id) {
+  editId = id;
+
+  document.querySelector(".create_form").style.display = "none";
+  document.querySelector(".update_form").style.display = "block";
+
+  let object = data.find((obj) => obj.id === id);
+  document.querySelector(".update_form .name").value = object.name;
+}
+
+function update() {
+  let updatedName = document.querySelector(".update_form .name").value;
+
+  let obj = data.find((item) => item.id === editId);
+  obj.name = updatedName;
+
+  readAll();
+
+  document.querySelector(".update_form").style.display = "none";
+  document.querySelector(".create_form").style.display = "block";
+  document.querySelector(".update_form .name").value = "";
+}
+
+function create() {
+  let name = document.querySelector(".create_form .name").value;
+
+  let obj = {
+    id: Date.now(), 
+    name: name,
+  };
+
+  data.push(obj);
+  readAll();
+
+  document.querySelector(".create_form .name").value = "";
+}
+
+function remove(id) {
+  data = data.filter((obj) => obj.id !== id);
+  readAll();
+}
