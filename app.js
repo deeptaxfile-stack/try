e74327273dd8dc5acdde49fc1f4386bf94dec936
deeptaxfile-stack <deep.taxfile@gmail.com>
2026-01-19 17:36:02 +0530

    try 2 validation add

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ function edit(id) {
 function update() {
   let updatedName = document.querySelector(".update_form .name").value;
 
+  if (updatedName.trim() === "") {
+    alert("Please enter a value to update!");
+    return;
+  }
+
   let obj = data.find((item) => item.id === editId);
   obj.name = updatedName;
 
@@ -43,6 +48,11 @@ function update() {
 
 function create() {
   let name = document.querySelector(".create_form .name").value;
+
+  if (name.trim() === "") {
+    alert("Please enter a value before submitting!");
+    return;
+  }
 
   let obj = {
     id: Date.now(), 
@@ -57,5 +67,14 @@ function create() {
 
 function remove(id) {
   data = data.filter((obj) => obj.id !== id);
+  
+
+  if (editId === id) {
+    editId = null;
+    document.querySelector(".update_form").style.display = "none";
+    document.querySelector(".create_form").style.display = "block";
+    document.querySelector(".update_form .name").value = "";
+  }
+  
   readAll();
 }
